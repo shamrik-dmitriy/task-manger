cd9a577c22b770d6e1d88a0e506de26064474352
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-06-16 00:01:25 +0600

    + Подключил генерацию Swagger

--- TM.WebApi/Program.cs
+++ TM.WebApi/Program.cs
@@ -9,8 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(optionsBuilder => { optionsBuilder.UseSqlite(connectionString); });
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
+app.UseSwagger()
+    .UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "TM.WebApi v1"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -25,32 +30,6 @@ using (var scope = app.Services.CreateScope())
 }
 
 app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapControllers();
 
 app.Run();
-
-namespace TM.WebApi
-{
-    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-    {
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-    }
-}
