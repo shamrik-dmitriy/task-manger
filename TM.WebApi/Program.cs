9ede7a6f50152f898d5c0bb87d6c1175dcd0aebd
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-06-23 00:00:14 +0600

    + Добавил валидацию

--- TM.WebApi/Program.cs
+++ TM.WebApi/Program.cs
@@ -1,26 +1,26 @@
+using System.Reflection;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
+using TM.Abstractions;
+using TM.Application.Interfaces;
+using TM.Application.Services;
 using TM.Persistance;
+using TM.Repositories;
 using TM.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(optionsBuilder => { optionsBuilder.UseSqlite(connectionString); });
-builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
+AddServices(builder.Services);
 var app = builder.Build();
-app.UseSwagger()
-    .UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "TM.WebApi v1"));
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger()
+        .UseSwaggerUI(options => options.SwaggerEndpoint("/swagger/v1/swagger.json", "TM.WebApi v1"));
 }
 
 using (var scope = app.Services.CreateScope())
@@ -33,3 +33,19 @@ app.UseHttpsRedirection();
 app.MapControllers();
 
 app.Run();
+
+void AddServices(IServiceCollection builderServices)
+{
+    builder.Services.AddOpenApi();
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services
+        .AddDbContext<ApplicationDbContext>(optionsBuilder => { optionsBuilder.UseSqlite(connectionString); })
+        .AddSwaggerGen()
+        .AddEndpointsApiExplorer()
+        .AddScoped<IUserRepository, UserRepository>()
+        .AddScoped<ITaskRepository, TaskRepository>()
+        .AddScoped<IUserService, UserService>()
+        .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
+        .AddFluentValidationAutoValidation()
+        .AddControllers();
+}
---
+++ TM.WebApi/Validation/CreateUserDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using TM.Contracts.DTOs.User;
+
+namespace TM.WebApi.Validation;
+
+public class CreateUserDtoValidator : AbstractValidator<CreateUserDto>
+{
+    public CreateUserDtoValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Электронная почта обязательна");
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Имя пользователя обязательно");
+        
+        RuleFor(x=>x.Password)
+            .Equal(x=>x.ConfirmPassword)
+            .WithMessage("Пароль и подтверждение не совпадают");
+    }
+}
