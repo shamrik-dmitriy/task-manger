2dd97802058747c4ecad87a6e6c197eee03de4ee
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-06-29 20:16:16 +0600

    ** Добавил комментариев

--- TM.Contracts/DTOs/User/CreateUserDto.cs
+++ TM.Contracts/DTOs/User/CreateUserDto.cs
@@ -2,9 +2,23 @@
 
 public class CreateUserDto
 {
+    /// <summary>
+    /// Псевдоним
+    /// </summary>
     public string Name { get; set; }
+    
+    /// <summary>
+    /// Адрес электронной почты
+    /// </summary>
     public string Email { get; set; }
+    
+    /// <summary>
+    /// Пароль
+    /// </summary>
     public string Password { get; set; }
     
+    /// <summary>
+    /// Подтверждение пароля
+    /// </summary>
     public string ConfirmPassword { get; set; }
 }
--- TM.Contracts/DTOs/User/UserDto.cs
+++ TM.Contracts/DTOs/User/UserDto.cs
@@ -2,7 +2,18 @@
 
 public class UserDto
 {
+    /// <summary>
+    /// Идентификатор
+    /// </summary>
     public Guid Id { get; set; }
+    
+    /// <summary>
+    /// Псевдоним
+    /// </summary>
     public string Name { get; set; }
+    
+    /// <summary>
+    /// Адрес электронной почты
+    /// </summary>
     public string Email { get; set; }
 }
