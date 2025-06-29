5c21e13ec7c7346a2c607ecdb94e4ea0322e2766
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-06-29 20:17:25 +0600

    ** Обновил контроллер для работы с Users

--- TM.WebApi/Controllers/UsersController.cs
+++ TM.WebApi/Controllers/UsersController.cs
@@ -18,27 +18,39 @@ public class UsersController : ControllerBase
     {
         _userService = userService;
     }
-    
+
     /// <summary>
-    /// Получить пользователя
+    /// Возвращает пользователя по идентификатору
     /// </summary>
-    /// <param name="id"></param>
-    /// <returns></returns>
+    /// <param name="id">Идентификатор пользователя</param>
+    /// <returns>Пользователь с идентификатором <see cref="id"/></returns>
     [HttpGet("{id}")]
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get(Guid id)
     {
         var user = await _userService.GetUserAsync(id);
         return user is null ? NotFound() : Ok(user);
     }
+
+    /// <summary>
+    /// Возвращает список существующих пользователей
+    /// </summary>
+    /// <returns>Существующие пользователи</returns>
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<UserDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Get()
     {
         var user = await _userService.GetUsersAsync();
         return user is null ? NotFound() : Ok(user);
     }
-    
+
     [HttpPost]
-    public async Task<IActionResult> Create([FromBody]CreateUserDto userDto)
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(UserDto), StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> Create([FromBody] CreateUserDto userDto)
     {
         var createdUser = await _userService.CreateUserAsync(userDto);
         return Ok(createdUser);
