de8b0c152c72f4a387f6741268c2cd197c463de7
shamrik-dmitriy <@FscHRgA+Hfvr2pa>
2025-06-29 22:50:04 +0600

    + Добавил метод расширения для маппинга

---
+++ TM.Application/Extensions/UserMappingExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TM.Contracts.DTOs.User;
+using TM.Domain.Entities.User;
+
+namespace TM.Application.Extensions;
+
+public static class UserMappingExtensions
+{
+    public static UserDto ToUserDto(this User user)
+    {
+        return new UserDto()
+        {
+            Id = user.Id,
+            Email = user.Email,
+            Name = user.Name
+        };
+    }
+}
